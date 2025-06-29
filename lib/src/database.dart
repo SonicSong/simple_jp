2d39fcd8bde3d6982771e3900b1612659e5f753c
Robert Starosta <robik2k4@gmail.com>
2025-06-29 23:27:36 +0200

    Database

--- lib/src/database.dart
+++ lib/src/database.dart
@@ -16,8 +16,14 @@ class SqlDbCreate {
           await database.execute('');
     });
   }
+
+  Future<List<Map<String, Object?>>> addQuestion(String category, String plText, String jpText, String romaji, String note) async {
+    final database = await initializeDatabase();
+    return await database.rawQuery('INSERT INTO phrases(category, pl_text, jp_text, romaji, notes) VALUES (?, ?, ?, ?, ?)', [category, plText, jpText, romaji, note]);
+  }
 }
 
+//For making requests to the database make use of category to not pull too much data at once. For example use 'Directions' to get all questions related to directions and not all questions.
 
 //Example of question
 // {
@@ -25,4 +31,5 @@ class SqlDbCreate {
 // "pl_text": "Gdzie jest sklep?",
 // "jp_text": "コンビニはどこにありますか？",
 // "romaji": "Konbini wa doko ni arimasu ka?"
+// "notes": "Use this phrase when asking for the location of a convenience store."
 // },
